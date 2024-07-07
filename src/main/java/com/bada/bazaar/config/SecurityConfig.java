2a958b62211632af5591c52a2566ea05b2f42118
Amol Nakhate <amol64546@gmail.com>
2024-07-07 10:03:51 +0530

    update securityConfig and swagger property

--- src/main/java/com/bada/bazaar/config/SecurityConfig.java
+++ src/main/java/com/bada/bazaar/config/SecurityConfig.java
@@ -12,25 +12,41 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+  private static final String[] WHITE_LIST_URL = {
+    "/v1/auth/**",
+    "/v2/api-docs",
+    "/v3/api-docs",
+    "/v3/api-docs/**",
+    "/swagger-resources",
+    "/swagger-resources/**",
+    "/configuration/ui",
+    "/configuration/security",
+    "/swagger-ui/**",
+    "/webjars/**",
+    "/swagger-ui.html"
+  };
 
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
   private final AuthenticationProvider authenticationProvider;
   private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
   private final CustomAccessDeniedHandler customAccessDeniedHandler;
+//  private final LogoutHandler logoutHandler;
 
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http
+    http
       .cors(AbstractHttpConfigurer::disable)
       .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(auth -> auth
-//        .requestMatchers("/v1/auth/**", "/swagger/**").permitAll()
+//        .requestMatchers(WHITE_LIST_URL).permitAll()
 //        .anyRequest().authenticated()
-        .anyRequest().permitAll()
+          .anyRequest().permitAll()
       )
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authenticationProvider(authenticationProvider)
@@ -39,11 +55,16 @@ public class SecurityConfig {
 //        .authenticationEntryPoint(customAuthenticationEntryPoint)
 //        .accessDeniedHandler(customAccessDeniedHandler)
 //      )
-      .build();
+//      .logout(logout ->
+//        logout.logoutUrl("/v1/auth/logout")
+//          .addLogoutHandler(logoutHandler)
+//          .logoutSuccessHandler(
+//            (request, response, authentication) -> SecurityContextHolder.clearContext())
+//      )
+    ;
 
-
+    return http.build();
   }
-
 
 
 }
